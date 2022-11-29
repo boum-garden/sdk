9141f003e12b9f1d4f8f92c448555cdbc92d577e
Marc Pfander <marc.pfander@gmail.com>
2022-11-29 19:44:43 +0800

    improves tests

--- tests/end2end_tests/api_client/v1/test__client.py
+++ tests/end2end_tests/api_client/v1/test__client.py
@@ -133,3 +133,11 @@ class TestUsersEndpointGet:
     def test__with_user_id__works(self, client):
         with client:
             result = client.root.users(USER_ID).get()
+
+        assert isinstance(result, dict)
+    def test__with_wrong_user_id__raises_http_error(self, client):
+        with client, pytest.raises(HTTPError):
+            client.root.users('some_user_id').get()
+
+
+
